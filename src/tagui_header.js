e628f3290a4893a376fd08379657dca1fad6e340
Ken Soh <ken@tebel.org>
2017-02-14 03:44:18 +0800

    adding hybrid xpath / css selection

--- src/tagui_header.js
+++ src/tagui_header.js
@@ -13,49 +13,73 @@ var automation_start_time = Date.now(); casper.echo('\nSTART - automation starte
 // initialise other global variables
 var save_text_count = 0; var snap_image_count = 0;
 
-// for muting echo in test automation scripts
+// muting echo in test automation scripts
 function dummy_echo(muted_string) {return;}
 
-// for saving text information to file
+// saving text information to file
 function save_text(file_name,info_text) {
 if (!file_name) {save_text_count++; file_name = 'text' + save_text_count.toString() + '.txt';}
 var fs = require('fs'); fs.write(file_name, info_text, 'w');}
 
-// for saving snapshots of website to file
+// saving snapshots of website to file
 function snap_image() {snap_image_count++; return ('image' + snap_image_count.toString() + '.png');}
 
-// for adding synchronous suspend capability
+// adding synchronous suspend capability
 function sleep(delay_in_ms) {var start_time = new Date().getTime();
 for (var sleep_count = 0; sleep_count < 1e7; sleep_count++)
 {if ((new Date().getTime() - start_time) > delay_in_ms) break;}}
 
-// for checking if selector is css selector
+// checking if selector is css selector
 function is_css_selector(selector) {
+if (selector.length == 0) return false; if (selector.charAt(0) == '[') return true;
+if (selector.charAt(0) == '.') return true; if (selector.charAt(0) == '#') return true;
+if (selector.substring(0,4) == 'div ') return true; if (selector.substring(0,4) == 'div:') return true;
+if (selector.substring(0,4) == 'div[') return true; if (selector.substring(0,2) == 'p ') return true;
+if (selector.substring(0,2) == 'p:') return true; if (selector.substring(0,2) == 'p[') return true;
+if (selector.substring(0,6) == 'input ') return true; if (selector.substring(0,6) == 'input:') return true;
+if (selector.substring(0,6) == 'input[') return true; if (selector.substring(0,7) == 'button ') return true;
+if (selector.substring(0,7) == 'button:') return true; if (selector.substring(0,7) == 'button[') return true;
+if (selector.substring(0,3) == 'li ') return true; if (selector.substring(0,3) == 'li:') return true;
+if (selector.substring(0,3) == 'li[') return true; if (selector.substring(0,3) == 'tr ') return true;
+if (selector.substring(0,3) == 'tr:') return true; if (selector.substring(0,3) == 'tr[') return true;
+if (selector.substring(0,5) == 'span ') return true; if (selector.substring(0,5) == 'span:') return true;
+if (selector.substring(0,5) == 'span[') return true;
+if (selector.substring(0,3) == 'h1 ') return true; if (selector.substring(0,3) == 'h1:') return true;
+if (selector.substring(0,3) == 'h1[') return true; if (selector.substring(0,3) == 'h2 ') return true;
+if (selector.substring(0,3) == 'h2:') return true; if (selector.substring(0,3) == 'h2[') return true;
+if (selector.substring(0,3) == 'h3 ') return true; if (selector.substring(0,3) == 'h3:') return true;
+if (selector.substring(0,3) == 'h3[') return true; if (selector.substring(0,3) == 'h4 ') return true;
+if (selector.substring(0,3) == 'h4:') return true; if (selector.substring(0,3) == 'h4[') return true;
+if (selector.substring(0,3) == 'h5 ') return true; if (selector.substring(0,3) == 'h5:') return true;
+if (selector.substring(0,3) == 'h5[') return true; if (selector.substring(0,3) == 'h6 ') return true;
+if (selector.substring(0,3) == 'h6:') return true; if (selector.substring(0,3) == 'h6[') return true;
 return false;}
- 
+
+// checking if selector is xpath selector
+function is_xpath_selector(selector) {
+if (selector.length == 0) return false; if (selector.indexOf('/') >= 0) return true; return false;}
+
 // finding best match for given locator
 function tx(locator) {
-// check if locator is css selector before going to xpath check sequence
+if (is_xpath_selector(locator)) return x(locator);
 if (is_css_selector(locator)) return locator;
-if (casper.exists(x(locator))) return x(locator);
 if (casper.exists(x('//*[contains(@id,"'+locator+'")]'))) return x('//*[contains(@id,"'+locator+'")]');
 if (casper.exists(x('//*[contains(@name,"'+locator+'")]'))) return x('//*[contains(@name,"'+locator+'")]');
 if (casper.exists(x('//*[contains(@class,"'+locator+'")]'))) return x('//*[contains(@class,"'+locator+'")]');
 if (casper.exists(x('//*[contains(@title,"'+locator+'")]'))) return x('//*[contains(@title,"'+locator+'")]');
 if (casper.exists(x('//*[contains(text(),"'+locator+'")]'))) return x('//*[contains(text(),"'+locator+'")]');
-else return x('/html');}
+return x('/html');}
 
 // checking if given locator is found
 function check_tx(locator) {
-// check if locator is css selector before going to xpath check sequence
+if (is_xpath_selector(locator)) {if (casper.exists(x(locator))) return true; else return false;}
 if (is_css_selector(locator)) {if (casper.exists(locator)) return true; else return false;}
-if (casper.exists(x(locator))) return true;
 if (casper.exists(x('//*[contains(@id,"'+locator+'")]'))) return true;
 if (casper.exists(x('//*[contains(@name,"'+locator+'")]'))) return true;
 if (casper.exists(x('//*[contains(@class,"'+locator+'")]'))) return true;
 if (casper.exists(x('//*[contains(@title,"'+locator+'")]'))) return true;
 if (casper.exists(x('//*[contains(text(),"'+locator+'")]'))) return true;
-else return false;}
+return false;}
 
 // calling rest api url synchronously
 function call_api(rest_url) {
