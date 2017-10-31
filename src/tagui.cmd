64552e8493a43f7b698737b2dd9226c74febf8bd
Ken Soh <ken@tebel.org>
2017-11-01 01:45:31 +0800

    add delay btw repetitions to pace out (and avoid system overheat)
    this is applied for macOS and Linux, but not for Windows as there is
    extra dependency for ping command.
    
    if there is a strong case for this on Windows, then the extra
    dependency on ping.exe can be explored.

--- src/tagui.cmd
+++ src/tagui.cmd
@@ -448,6 +448,9 @@ rem big loop for managing multiple data sets in datatable
 for /l %%n in (1,1,%tagui_data_set_size%) do (
 set tagui_data_set=%%n
 
+rem add delay between repetitions to avoid system overheat
+rem skip for windows as there is extra dependency on ping
+
 rem parse automation flow file, check for initial parse error, check sikuli and chrome, before calling casperjs
 php -q tagui_parse.php "%flow_file%" | tee -a "%flow_file%.log"
 for /f "usebackq" %%f in ('%flow_file%.log') do set file_size=%%~zf
