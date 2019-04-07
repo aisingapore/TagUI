b44b261e1480915e3c0b2e330ad921bc8e757436
Ken Soh <kensoh@gmail.com>
2019-04-07 18:28:03 +0800

    #384 - run results auditing and tracking enhancement to report option
    Sending a PR to enhance report option. In current state when report option is used, TagUI will generate a HTML log file, in addition to the usual text log file.
    
    With this enhancement, when the report option is used, TagUI will track the run results in a csv spreadsheet (tagui/src/tagui_report.csv), along with html logs that persist with a running number.
    
    This allows users to explicitly decide which automation flows to be tracked and tabulated in tagui_report.csv. This information may be useful for personal reference, to measure time savings from automation, for keeping a trail for auditing purpose, or perhaps facilitate sending a summary to boss. Following is an example of how tagui_report.csv will look like (time taken in seconds) -
    
    #| AUTOMATION FLOW | START TIME | FINISH TIME | ERROR STATUS | LOG FILE
    :--|:--|:--|:--|:--|:--
    1 | /Users/kensoh/Desktop/download_flow | Sun Apr 07 2019 17:33:58 GMT+0800 (+08) | 32.1 | SUCCESS | /Users/kensoh/Desktop/download_flow_1.html
    2 | /Users/kensoh/Desktop/upload_flow | NOT STARTED | NOT FINISHED | [LINE 1] cannot understand step self-destruct | /Users/kensoh/Desktop/upload_flow_2.html
    3 | /Users/kensoh/Desktop/update_flow | NOT STARTED | NOT FINISHED | [LINE 1] cannot understand step reboot computer | /Users/kensoh/Desktop/update_flow_3.html
    4 | /Users/kensoh/Desktop/booking_flow | Sun Apr 07 2019 17:39:00 GMT+0800 (+08) | NOT FINISHED | cannot find confirm_booking | /Users/kensoh/Desktop/booking_flow_4.html

--- src/tagui_report.php
+++ src/tagui_report.php
@@ -8,6 +8,42 @@ if (!file_exists($script . '.log')) die("ERROR - cannot find " . $script . '.log
 $log_file = fopen($script . '.log','r') or die("ERROR - cannot open " . $script . '.log' . "\n"); fclose($log_file);
 $rep_file = fopen($script . '.html','w') or die("ERROR - cannot open " . $script . '.html' . "\n"); fclose($rep_file);
 
+// below log automation outcome to audit report csv database
+
+$log_content = file_get_contents($script . '.log');
+$start_timestamp = "NOT STARTED"; $timestamp_header = "START - automation started - ";
+if (strpos($log_content, $timestamp_header) !== false) {
+$startpos_timestamp = strpos($log_content, $timestamp_header) + strlen($timestamp_header);
+$start_timestamp = substr($log_content, $startpos_timestamp); $endpos_timestamp = strpos($start_timestamp, "\n");
+$start_timestamp = trim(substr($start_timestamp, 0, $endpos_timestamp));}
+
+$finish_timestamp = "NOT FINISHED"; $timestamp_footer = "FINISH - automation finished - ";
+if (strpos($log_content, $timestamp_footer) !== false) {
+$startpos_timestamp = strpos($log_content, $timestamp_footer) + strlen($timestamp_footer);
+$finish_timestamp = substr($log_content, $startpos_timestamp); $endpos_timestamp = strpos($finish_timestamp, "\n");
+$finish_timestamp = substr(trim(substr($finish_timestamp, 0, $endpos_timestamp)),0,-1);}
+
+$error_status = "SUCCESS"; $error_identifier = "ERROR - "; 
+if (strpos($log_content, $error_identifier) !== false) {
+$startpos_error = strpos($log_content, $error_identifier) + strlen($error_identifier);
+$error_status = substr($log_content, $startpos_error); $endpos_error = strpos($error_status, "\n");
+$error_status = trim(substr($error_status, 0, $endpos_error));}
+
+// escape double quotes in the csv data values
+$escaped_script = str_replace('"', '""', $script);
+$start_timestamp = str_replace('"', '""', $start_timestamp);
+$finish_timestamp = str_replace('"', '""', $finish_timestamp);
+$error_status = str_replace('"', '""', $error_status);
+
+$audit_output_header = '"#","AUTOMATION FLOW","START TIME","FINISH TIME","ERROR STATUS","LOG FILE"';
+if (!file_exists('tagui_report.csv')) file_put_contents('tagui_report.csv', $audit_output_header . "\r\n");
+$audit_output_count = count(file('tagui_report.csv')) - 1 + 1; // to track and increment entry # in audit file
+$html_log_file = $script . '_' . $audit_output_count . '.html'; // to allow log persistence without overwriting
+$audit_output_line = '"' . $audit_output_count . '","' . $escaped_script . '","' . $start_timestamp . '","'; 
+$audit_output_line .= $finish_timestamp . '","' . $error_status . '","' . $html_log_file . '"' . "\r\n";
+$audit_output_file = fopen('tagui_report.csv','a') or die("ERROR - cannot open " . 'tagui_report.csv' . "\n");
+fwrite($audit_output_file, $audit_output_line); fclose($audit_output_file);
+
 // below start conversion of text log file to html file
 
 // add html <br> tag to newline for proper line breaks
@@ -26,5 +62,6 @@ $log_content = "<!DOCTYPE html>\n<html><head>" .
 "<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:600' rel='stylesheet'>\n" . 
 "<style>body {font-family: 'Source Sans Pro', sans-serif; width: 90%; margin: auto;}</style>" . 
 "</head><body><h3>\n" . $log_content . "</h3></body></html>"; file_put_contents($script . '.html',$log_content);
+file_put_contents($script . '_' . $audit_output_count . '.html',$log_content); // write another file that persists
 
 ?>
