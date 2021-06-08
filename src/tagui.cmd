54415cbc11917445c90a30818f0fd43f941ba6d4
Ken Soh <kensoh@gmail.com>
2021-06-08 14:26:01 +0800

    #1053 - make Windows processes titles user-friendly

--- src/tagui.cmd
+++ src/tagui.cmd
@@ -14,7 +14,7 @@ rem enable windows for loop advanced flow control
 setlocal enableextensions enabledelayedexpansion
 
 if "%~1"=="" (
-echo tagui v6.43: use following options and this syntax to run - tagui flow_filename option^(s^)
+echo tagui v6.44: use following options and this syntax to run - tagui flow_filename option^(s^)
 echo.
 echo tagui live     launch TagUI live mode enabled with visual automation for interactive development
 echo input^(s^)       add your own parameter^(s^) to be used in your automation flow as variables p1 to p8
@@ -932,22 +932,22 @@ if exist "%flow_file%.js" (
 
 rem start R process if integration file is created during parsing
 if exist "tagui_r\tagui_r.in" (
-        start /min cmd /c Rscript tagui_r\tagui_r.R 2^>^&1 ^| tee -a tagui_r\tagui_r.log
+        start "R Engine" /min cmd /c Rscript tagui_r\tagui_r.R 2^>^&1 ^| tee -a tagui_r\tagui_r.log
 )
 
 rem start python process if integration file is created during parsing
 if exist "tagui_py\tagui_py.in" (
-        start /min cmd /c python -u tagui_py\tagui_py.py 2^>^&1 ^| tee -a tagui_py\tagui_py.log
+        start "Python Engine" /min cmd /c python -u tagui_py\tagui_py.py 2^>^&1 ^| tee -a tagui_py\tagui_py.log
 )
 
 rem start sikuli process if integration file is created during parsing
 if exist "tagui.sikuli\tagui_sikuli.in" (
 	rem start dummy first run to let sikulix integrate jython
 	if exist "sikulix\jython-standalone-2.7.1.jar" (
-		start /min cmd /c java -jar sikulix\sikulix.jar -h ^> nul 2^>^&1
+		start "SikuliX Engine" /min cmd /c java -jar sikulix\sikulix.jar -h ^> nul 2^>^&1
 	)
 	rem echo [starting sikuli process] | tee -a "%flow_file%.log"
-	start /min cmd /c java -jar sikulix\sikulix.jar -r tagui.sikuli -d 3 2^>^&1 ^| tee -a tagui.sikuli\tagui.log
+	start "SikuliX Engine" /min cmd /c java -jar sikulix\sikulix.jar -r tagui.sikuli -d 3 2^>^&1 ^| tee -a tagui.sikuli\tagui.log
 )
 
 rem start chrome processes if integration file is created during parsing
@@ -992,7 +992,7 @@ if exist "tagui_chrome.in" (
 	)
 
 	rem launch php process to manage chrome websocket communications
-	start /min cmd /c php -q tagui_chrome.php !ws_url! ^| tee -a tagui_chrome.log
+	start "Chrome Engine" /min cmd /c php -q tagui_chrome.php !ws_url! ^| tee -a tagui_chrome.log
 
 rem end of if block to start chrome processes
 )
